735f2430a1a033e93e66918ccc4576403dfc9a40
dflk <904885508@qq.com>
2019-05-12 22:56:00 +0800

    db

--- script/db.js
+++ script/db.js
@@ -4,64 +4,70 @@
  */
 (function(window){
     var u = {};
-    var db
-    var dbName = 'xudong'
-    var dbPath = 'fs://xudong.db'
-    var tableName = 'shoppingCart'
+    
+    var db;
+    var dbName = 'missfresh_demo';
+    var dbPath = 'fs://missfresh_demo.db';
+    var tableName = 'shoppingCart';
 
-
+    // 引入db模块
     u.init = function(){
-      if(db){
-        return
-      }
-      db = api.require("db");
-    }
+        if (db) {
+            return;
+        }
+        db = api.require('db');
+    };
 
-    u.open = function(){
-      u.init()
-      var ret = db.openDatabaseSync({
-          name: dbName,
-          path: dbPath
-      });
-      if(!ret.status){
-        db.executeSqlSync({
+    // 创建数据库及相关的数据表结构
+    u.open = function() {
+        u.init();
+        var ret = db.openDatabaseSync({
             name: dbName,
-            sql: 'CREATE TABLE' + tableName + '(wareId varchar(32), wareCount INT'
+            path: dbPath
         });
-      }
+        if (ret.status) {
+            db.executeSqlSync({
+                name: dbName,
+                sql: 'CREATE TABLE ' + tableName + '(wareId VARCHAR(32), wareCount INT)'   
+            });
+        }
     }
 
-    u.select = function(wareId_){ // 按照id查询
-      u.init()
-      var sql = "SELECT * FROM" + tableName  // 不传id就查询全部数据
-      if(wareId_){
-        sql = 'SELECT * FROM' + tableName + 'WHERE wareId=\"' + wareId_ + '\"'
-      }
-      return db.selectSqlSync({
-          name: dbName,
-          sql: sql
-      });
+    // 查询操作，查询table中全部数据，或按商品ID查询
+    u.select = function(wareId_) {
+        u.init();
+        var sql = 'SELECT * FROM ' + tableName;
+        if (wareId_) {
+            sql = 'SELECT * FROM ' + tableName + ' WHERE wareId=\"' + wareId_ + '\"';
+        }
+        return db.selectSqlSync({
+            name: dbName,
+            sql: sql
+        });
     }
 
-    u.update = function(wareId_, wareCount_){
-      u.init()
-      db.executeSqlSync({
-          name: dbName,
-          // 当wareId = wareId_ 更新wareCount = wareCount_
-          sql: 'UPDATE' + tableName 'SET wareCount=' + wareCount_ 'WHERE wareId=\"' + wareId_+ '\"'
-      });
-
+    // 更新操作，更新购物车中指定ID的商品数量
+    u.update = function(wareId_, wareCount_) {
+        u.init();
+        return db.executeSqlSync({
+            name: dbName,
+            sql: 'UPDATE ' + tableName + ' SET wareCount=' + wareCount_ + ' WHERE wareId=\"' + wareId_ + '\"'
+        });
     }
 
-    u.insert = function(wareId_, wareCount_){
-      u.init()
-      return db.executeSqlSync({
-          name: dbName,
-          sql: 'INSERT INTO' + tableName + '(wareId_, wareCount_) VALUES(\"' + wareId_ + '\", wareCount_)'
-      });
-
+    // 插入操作，将商品ID和数量添加到购物车中
+    u.insert = function(wareId_, wareCount_) {
+        u.init();
+        return db.executeSqlSync({
+            name: dbName,
+            sql: 'INSERT INTO ' + tableName + '(wareId,wareCount) VALUES(\"' + wareId_ + '\",' + wareCount_ + ')'    
+        });
     }
 
-    window.$api = u;
+/*end*/
+    
+    window.$db = u;
 
 })(window);
+
+
